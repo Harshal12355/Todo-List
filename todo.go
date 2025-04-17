946b7ba2bcca77b966743095acba14d7c9247414
Harshal12355 <harshalts@gmail.com>
2025-04-17 12:40:19 -0400

    added functions for adding, removing, and printing out tasks

--- todo.go
+++ todo.go
@@ -2,20 +2,52 @@ package main
 
 import "fmt"
 
-// func tasks(task){
-	
-// }
+// Functions in Go
+
+func printTasks(taskItemsSlice []string){ // printing tasks 
+	fmt.Println("List of my Todos")
+	for index, task := range taskItemsSlice {
+		index = index + 1
+		fmt.Printf("%d. %s\n", index, task)
+	}
+}
+
+//When returning a value, make sure to specify what type of value are you returning, in the case for the functions below, they are returning a string slice 
+func addTask(task string, taskItemsSlice []string) []string { // function to add a task to the array or slice
+	var updatedItems = append(taskItemsSlice, task) // append the task to the slice and return the new slice
+	printTasks(updatedItems)
+	return updatedItems
+} // end of addTask function
+
+func removeTask(index int, taskItemsSlice []string) []string { // function to remove a task given the index 
+	index = index - 1
+	var updatedItems = append(taskItemsSlice[:index], taskItemsSlice[index+1:]...) // removing a value from a specified index
+	printTasks(updatedItems)
+	return updatedItems
+}
 
 func main() {
-	var taskOne = "Watch go lang tutorial"
-	var taskItems = []string{
-		taskOne,
-		taskOne,
-		taskOne,
-	}
-
+	// Printing statements 
 	fmt.Println("Welcome to TODOist!")
+
+	// Declaring variables 
+	var taskOne = "Watch go lang tutorial in a slice" // declaring a variable (you should be able to lock in what the data type for a variable can be as well)
+	var taskTwo = "Know what the difference betweeen an array and a slice is" // declaring second variable 
+	var taskThree = "Know how to declare different kinds of arrays and slices"
+	var taskFour = "This is task 4"
+	// Slices and Arrays 
+	var taskItemsSlice = []string{
+		taskOne,
+		taskTwo,
+		taskThree,
+	} // declaring a string slice, can have unlimited values inside it
 	
 	fmt.Println("Todo List:")
-	fmt.Println(taskItems[2])
-}
+	printTasks(taskItemsSlice)
+	fmt.Println("Added a new task")
+	taskItemsSlice = addTask(taskFour, taskItemsSlice)
+	
+
+	fmt.Println("Removed a task")
+	taskItemsSlice = removeTask(3, taskItemsSlice)
+} // end of main function
